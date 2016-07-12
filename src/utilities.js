4a13b3c4f53709f9539a3935deb8697b2a649708
Isaac Suttell <isaac_suttell@playstation.sony.com>
2016-07-12 11:23:03 -0700

    added UtilityComponent

--- src/utilities.js
+++ src/utilities.js
@@ -1,3 +1,5 @@
+import {Component} from 'react';
+
 /**
  * Generate a hash from a string used for shorter keys
  * @param  {string} str [description]
@@ -14,4 +16,51 @@ export function hashCode(str) {
     hash |= 0; // Convert to 32bit integer
   }
   return hash;
+}
+
+/**
+ * Adds some utility functions to React.Component
+ */
+export class UtilityComponent extends Component {
+    /**
+     * Create a function that can only be called every `threshold`
+     * @param  {Function} fn
+     * @param  {Number}   threshold    default to 200, or a response ajax request time
+     * @param  {Mixed}    ctx          default to this instance
+     * @return {Function}
+     */
+    throttleFn(fn, threshold = 200, ctx = this) {
+      if (typeof this.throttleIds !== 'object') {
+        this.throttleIds = {};
+      }
+      let last;
+      return function(){
+        let now = Date.now();
+        let args = arguments;
+        if (last && now < last + threshold) {
+          clearTimeout(this.throttleIds[fn.name]);
+          this.throttleIds[fn.name] = setTimeout(function(){
+            last = now;
+            fn.apply(ctx, args);
+          }, threshold)
+        } else {
+          last = now;
+          fn.apply(ctx, args);
+        }
+      }.bind(this);
+    }
+
+    /**
+     * Clean up any straggling functions
+     */
+    clearThrottleFn() {
+      if (typeof this.throttleIds !== 'object') {
+        return;
+      }
+      for(var key in this.throttleIds) {
+        if(this.throttleIds.hasOwnProperty(key)) {
+          clearTimeout(this.throttleIds[key]);
+        }
+      }
+    }
 }
